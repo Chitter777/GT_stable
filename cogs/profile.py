cea4c9b0ea804218b364c7f24c51e322108aff7d
Chitter777 <50046751+Chitter777@users.noreply.github.com>
2022-04-29 19:06:30 +0700

    Временная уборка ПИЛа из профиля

--- cogs/profile.py
+++ cogs/profile.py
@@ -7,7 +7,7 @@ from disnake.ext import commands
 import sqlite3
 #import os.path
 from brawlstats import Client
-from PIL import Image, ImageFont, ImageDraw
+#from PIL import Image, ImageFont, ImageDraw
 import requests
 from datetime import datetime
 import time
@@ -76,27 +76,27 @@ class profile(commands.Cog):
                 tag = None
             embed = disnake.Embed(description="Информация об участнике:", color=0x5865F2)
             embed.set_author(name=f"Профиль {user.name}#{user.discriminator}", icon_url=user.avatar)
-            img = Image.new('RGBA', (900, 200), '#5865F2')
-            avatar = str(user.avatar)[:-10]
-            response = requests.get(avatar, stream = True)
-            response = Image.open(io.BytesIO(response.content))
-            response = response.convert('RGBA')
-            response = response.resize((160, 160), Image.ANTIALIAS)
-            img.paste(response, (20, 20, 180, 180))
-            idraw = ImageDraw.Draw(img)
-            if user.nick is not None:
-                name = user.nick
-            else:
-                name = user.name
+            #img = Image.new('RGBA', (900, 200), '#5865F2')
+            #avatar = str(user.avatar)[:-10]
+            #response = requests.get(avatar, stream = True)
+            #response = Image.open(io.BytesIO(response.content))
+            #response = response.convert('RGBA')
+            #response = response.resize((160, 160), Image.ANTIALIAS)
+            #img.paste(response, (20, 20, 180, 180))
+            #idraw = ImageDraw.Draw(img)
+            #if user.nick is not None:
+                #name = user.nick
+            #else:
+                #name = user.name
             #disc = user.discriminator
-            headline = ImageFont.truetype('Pusia-Bold.ttf', size=36)
-            undertext = ImageFont.truetype('Pusia-Bold.ttf', size=24)
+            #headline = ImageFont.truetype('Pusia-Bold.ttf', size=36)
+            #undertext = ImageFont.truetype('Pusia-Bold.ttf', size=24)
             # idraw.text((210, 50), f"{name}#{disc}", font=headline)
-            idraw.text((210, 50), f"{name}", font=headline)
+            #idraw.text((210, 50), f"{name}", font=headline)
             #if teamId != None:
             #    idraw.text((180, 25), f"Команда {teamna}", font=undertext)
-            img.save('user_card.png')
-            embed.set_image(file = disnake.File("user_card.png"))
+            #img.save('user_card.png')
+            #embed.set_image(file = disnake.File("user_card.png"))
             dr = user.created_at
             drts = str(dr.timestamp() * 1000)
             embed.add_field(name="Информация об аккаунте Discord:", value=f"Дата регистрации: <t:{drts[:-5]}:f>(<t:{drts[:-5]}:R>)", inline=True)
@@ -182,27 +182,27 @@ class profile(commands.Cog):
                 tag = None
             embed = disnake.Embed(description="Информация об участнике:", color=0x5865F2)
             embed.set_author(name=f"Профиль {user.name}", icon_url=user.avatar)
-            img = Image.new('RGBA', (900, 200), '#5865F2')
-            avatar = str(user.avatar)[:-10]
-            response = requests.get(avatar, stream=True)
-            response = Image.open(io.BytesIO(response.content))
-            response = response.convert('RGBA')
-            response = response.resize((160, 160), Image.ANTIALIAS)
-            img.paste(response, (20, 20, 180, 180))
-            idraw = ImageDraw.Draw(img)
-            if user.nick is not None:
-                name = user.nick
-            else:
-                name = user.name
+            #img = Image.new('RGBA', (900, 200), '#5865F2')
+            #avatar = str(user.avatar)[:-10]
+            #response = requests.get(avatar, stream=True)
+            #response = Image.open(io.BytesIO(response.content))
+            #response = response.convert('RGBA')
+            #response = response.resize((160, 160), Image.ANTIALIAS)
+            #img.paste(response, (20, 20, 180, 180))
+            #idraw = ImageDraw.Draw(img)
+            #if user.nick is not None:
+                #name = user.nick
+            #else:
+                #name = user.name
             #disc = user.discriminator
-            headline = ImageFont.truetype('Pusia-Bold.ttf', size=36)
-            undertext = ImageFont.truetype('Pusia-Bold.ttf', size=24)
+            #headline = ImageFont.truetype('Pusia-Bold.ttf', size=36)
+            #undertext = ImageFont.truetype('Pusia-Bold.ttf', size=24)
             #idraw.text((210, 50), f"{name}#{disc}", font=headline)
             idraw.text((210, 50), f"{name}", font=headline)
             # if teamId != None:
             #    idraw.text((180, 25), f"Команда {teamna}", font=undertext)
-            img.save('user_card.png')
-            embed.set_image(file=disnake.File("user_card.png"))
+            #img.save('user_card.png')
+            #embed.set_image(file=disnake.File("user_card.png"))
             dr = user.created_at
             drts = str(dr.timestamp() * 1000)
             embed.add_field(name="Информация об аккаунте Discord:", value=f"Дата регистрации: <t:{drts[:-5]}:f>(<t:{drts[:-5]}:R>)", inline=True)
