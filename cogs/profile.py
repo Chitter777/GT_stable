e94f742b533e01c6720073a5074c9dada2f49a4c
Chitter777 <50046751+Chitter777@users.noreply.github.com>
2022-05-03 16:09:11 +0700

    Update profile.py

--- cogs/profile.py
+++ cogs/profile.py
@@ -109,7 +109,7 @@ class profile(commands.Cog):
                 except:
                     embed.add_field(name=":warning: Проблемы с Brawl Stars API", value="Сейчас наблюдаются проблемы с BS API. Возможно, сейчас проводятся тенические работы.", inline=True)
             else:
-                if inter.author = user:
+                if inter.author == user:
                     embed.add_field(name="Совет:", value="Вы можете привязать тег с помощью `/привязать`, чтобы получить доступк статистике", inline=False)
 
             if icons == '1000':
