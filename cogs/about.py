84be224934482c265c38251df92fdc0f5ad36cb1
Chitter777 <50046751+Chitter777@users.noreply.github.com>
2022-05-03 15:42:33 +0700

    Оновление условий обслуживания

--- cogs/about.py
+++ cogs/about.py
@@ -8,20 +8,8 @@ class buttons(disnake.ui.View):
     def __init__(self):
         super().__init__()
         self.add_item(disnake.ui.Button(label="Сервер поддержки", url="https://discord.gg/esaPCQvpwY", emoji="<:chitty_bughunter1:925311594408312865>"))
-
-
-    @disnake.ui.button(emoji="<:chitty_rules:924980688531390506>", label="Условия использования", style=ButtonStyle.grey)
-    async def terms(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
-        embed = disnake.Embed(
-            title="Условия обслуживания Galka's Tournaments:",
-            description="""**1. Пользователь**(т.е. Вы) при первом взаимодействии с ботом подтверждаете, что вы согласны со всеми положениями **Условий обслуживания Galka's Tournaments**.
-  **2.** Вы обязуетесь не нарушать **[Условия использования Discord](https://discord.com/terms)** и правила **Supercell**.
-            
-  При нарушении вами **Условий обслуживания**, Мы __имеем полное право **лишить** вас **доступа к боту**__!""",
-            colour=0x5865F2
-        )
-        embed.set_footer(text="CHIT SQD | (C) 2022 | Все права взломаны", icon_url="https://cdn.discordapp.com/icons/794400771662676009/a_ab67786efe3ceccc550f4127e1992176.gif")
-        await interaction.send(embed=embed, ephemeral=True)
+        self.add_item(disnake.ui.Button(label="Условия использования", url="https://app.gitbook.com/s/qFGyL72altMmYs8IOmCS/important/tos", emoji="<:chitty_rules:924980688531390506>"))
+        
 
 class about(commands.Cog):
     def __init__(self, bot):
@@ -49,6 +37,8 @@ class about(commands.Cog):
                 colour=0x5865F2
             )
             embed.add_field(name="Разработчики:", value="<:chitter777:959425285470683158> Chitter777#0070\n<:kotik:959428879741780048> Кот? | kotik_nekot#9395", inline=True)
+            embed.add_field(name="Полезные ссылки:", value = """[Условия использования](https://app.gitbook.com/s/qFGyL72altMmYs8IOmCS/important/tos)
+[Документация](https://chitter777.gitbook.io/dokumentaciya-galkas-tournaments/)""", inline=True) 
             embed.set_footer(text="CHIT SQD | (C) 2022 | Все права взломаны", icon_url="https://cdn.discordapp.com/icons/794400771662676009/a_ab67786efe3ceccc550f4127e1992176.gif")
             view = buttons()
             await inter.send(embed=embed, view=buttons())
