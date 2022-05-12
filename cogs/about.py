41c7c1f89a74342d0895bfa905a1fe9e823a1f33
Chitter777 <50046751+Chitter777@users.noreply.github.com>
2022-05-12 16:36:43 +0700

    Продолжение банкета

--- cogs/about.py
+++ cogs/about.py
@@ -4,30 +4,42 @@ from disnake.ext import commands
 from disnake.enums import ButtonStyle
 import sqlite3
 
+
 class buttons(disnake.ui.View):
     def __init__(self):
         super().__init__()
         self.add_item(disnake.ui.Button(label="Сервер поддержки", url="https://discord.gg/esaPCQvpwY", emoji="<:chitty_bughunter1:925311594408312865>"))
         self.add_item(disnake.ui.Button(label="Условия использования", url="https://app.gitbook.com/s/qFGyL72altMmYs8IOmCS/important/tos", emoji="<:chitty_rules:924980688531390506>"))
-        
+
+class tool():
+    def isblocked(self, cur, inter):
+        try:
+            banReason, = cur.execute("SELECT banReason FROM usersd WHERE id == ?", (inter.author.id,)).fetchone()
+            return banReason
+        except:
+            return None
 
 class about(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("[LOAD] Ког about загружен успено!")
+
     @commands.slash_command(name='обомне', description="Информация о боте")
     async def about(self, inter):
         con = sqlite3.connect('bsdb.db')
         cur = con.cursor()
-        banReason, = cur.execute(f"SELECT banReason FROM usersd WHERE id = '{inter.author.id}'").fetchone()
-        if banReason != None:
+        bnRsn = tool.isblocked(self, cur=cur, inter=inter)
+        if bnRsn is not None:
             embed = disnake.Embed(
                 title=":no_entry_sign: Отказ в обслуживании",
                 description=f"Вам отказано в обслуживании!",
                 color=0xED4245
             )
             # embed.set_thumbnail(url="")
-            embed.add_field(name="Причина отказа:", value=f"{banReason}")
+            embed.add_field(name="Причина отказа:", value=f"{bnRsn}")
             embed.set_footer(text="Решение о блокировке обжалованию не подлежит!")
             await inter.send(embed=embed, ephemeral=True)
         else:
@@ -36,13 +48,17 @@ class about(commands.Cog):
                 description="Привет. Это приватный бот для турниров на сервере **Galka's Team**",
                 colour=0x5865F2
             )
-            embed.add_field(name="Разработчики:", value="<:chitter777:959425285470683158> Chitter777#0070\n<:kotik:959428879741780048> Кот? | kotik_nekot#9395", inline=True)
-            embed.add_field(name="Полезные ссылки:", value = """[Условия использования](https://app.gitbook.com/s/qFGyL72altMmYs8IOmCS/important/tos)
-[Документация](https://chitter777.gitbook.io/dokumentaciya-galkas-tournaments/)""", inline=True) 
-            embed.set_footer(text="CHIT SQD | (C) 2022 | Все права взломаны", icon_url="https://cdn.discordapp.com/icons/794400771662676009/a_ab67786efe3ceccc550f4127e1992176.gif")
+            embed.add_field(
+                name="Разработчики:",
+                value="<:chitter777:959425285470683158> Chitter777#0070\n<:kotik:959428879741780048> Кот? | kotik_nekot#9395",
+                inline=True
+            )
+            embed.add_field(name="Полезные ссылки:", value="""[Условия использования](https://app.gitbook.com/s/qFGyL72altMmYs8IOmCS/important/tos)
+[Документация](https://chitter777.gitbook.io/dokumentaciya-galkas-tournaments/)""", inline=True)
+            embed.set_footer(text="CHIT SQD | (C) 2022 | Все права взломаны",
+                             icon_url="https://cdn.discordapp.com/icons/794400771662676009/a_ab67786efe3ceccc550f4127e1992176.gif")
             view = buttons()
-            await inter.send(embed=embed, view=buttons())
-
+            await inter.send(embed=embed, view=view)
 
 
 def setup(bot):
